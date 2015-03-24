bc0cf25bb43732d18ca07d4f44aa80678ba37bb8
Alex Striff <alex.striff1@gmail.com>
2015-03-24 09:11:01 -0700

    Update 3sm.c

--- 3sm.c
+++ 3sm.c
@@ -3,5 +3,8 @@
 int main(int argc, const char *argv[])
 {
 	/* TODO: Parse argv options to suspend or hibernate, and at X% battery. */
+	
+	/* For now, just suspend. */
+	FILE *fd = fopen("w", "/sys/class/SUSPEND_FILE HERE (TODO)");
 	return 0;
 }
